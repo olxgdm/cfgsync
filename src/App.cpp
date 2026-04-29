6866a77c8b2f68d174102a82153f642e8c058550
Oleksii Gaidadim <agaidadim@gmail.com>
2026-04-29 12:32:20 +0200

    feat: Integrate AppConfig into main application flow

--- src/App.cpp
+++ src/App.cpp
@@ -7,6 +7,7 @@
 #include <CLI/CLI.hpp>
 
 #include "cli/BuildCli.hpp"
+#include "utils/AppConfigPath.hpp"
 
 namespace cfgsync {
 
@@ -16,9 +17,9 @@ int App::Run(int argc, char* argv[]) {
     cli.set_help_all_flag("--help-all", "Show help for all commands.");
     cli.set_version_flag("-v,--version", "cfgsync 0.1.0");
 
-    cli::BuildCli(cli, Registry_, StorageManager_);
-
     try {
+        AppConfig_.SetConfigPath(utils::GetDefaultAppConfigPath());
+        cli::BuildCli(cli, Registry_, StorageManager_, AppConfig_);
         cli.parse(argc, argv);
         return EXIT_SUCCESS;
     } catch (const CLI::ParseError& error) {
--- src/cli/BuildCli.hpp
+++ src/cli/BuildCli.hpp
@@ -2,11 +2,15 @@
 
 #include <CLI/CLI.hpp>
 
+#include "core/AppConfig.hpp"
 #include "core/Registry.hpp"
 #include "storage/StorageManager.hpp"
 
 namespace cfgsync::cli {
 
-void BuildCli(CLI::App& app, core::Registry& registry, storage::StorageManager& storageManager);
+void BuildCli(CLI::App& app,
+              core::Registry& registry,
+              storage::StorageManager& storageManager,
+              core::AppConfig& appConfig);
 
 }  // namespace cfgsync::cli
