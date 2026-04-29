77bf203249ac325f6f45ce5fb76ad3525c366b83
Oleksii Gaidadim <agaidadim@gmail.com>
2026-04-29 18:10:11 +0200

    test: Add PathFileUtilsTests and update CMakeLists.txt

---
+++ tests/PathFileUtilsTests.cpp
@@ -0,0 +1,155 @@
+#include "common/TestTempDirectory.hpp"
+#include "gtest/gtest.h"
+#include "utils/FileUtils.hpp"
+#include "utils/PathUtils.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <iterator>
+#include <stdexcept>
+#include <string>
+
+#ifdef _WIN32
+#include <cstdlib>
+#else
+#include <cstdlib>
+#endif
+
+namespace {
+namespace fs = std::filesystem;
+
+void WriteTextFile(const fs::path& path, const std::string& contents) {
+    if (path.has_parent_path()) {
+        cfgsync::utils::EnsureDirectoryExists(path.parent_path());
+    }
+
+    std::ofstream output{path};
+    output << contents;
+}
+
+std::string ReadTextFile(const fs::path& path) {
+    std::ifstream input{path};
+    return std::string{std::istreambuf_iterator<char>{input}, std::istreambuf_iterator<char>{}};
+}
+
+void SetEnvironmentVariable(const char* name, const std::string& value) {
+#ifdef _WIN32
+    _putenv_s(name, value.c_str());
+#else
+    setenv(name, value.c_str(), 1);
+#endif
+}
+
+class PathFileUtilsTest : public testing::Test {
+protected:
+    void SetUp() override { TestRoot = cfgsync::tests::MakeTestRoot(); }
+
+    void TearDown() override { fs::remove_all(TestRoot); }
+
+    fs::path GetTestRoot() const { return TestRoot; }
+
+private:
+    fs::path TestRoot;
+};
+
+TEST_F(PathFileUtilsTest, NormalizePathReturnsEmptyForEmptyInput) {
+    EXPECT_TRUE(cfgsync::utils::NormalizePath({}).empty());
+}
+
+TEST_F(PathFileUtilsTest, NormalizePathConvertsRelativeInputToAbsoluteNormalizedPath) {
+    const auto path = fs::path{"."} / "src" / ".." / "README.md";
+
+    const auto normalizedPath = cfgsync::utils::NormalizePath(path);
+
+    EXPECT_TRUE(normalizedPath.is_absolute());
+    EXPECT_EQ(normalizedPath, fs::absolute("README.md").lexically_normal());
+}
+
+TEST_F(PathFileUtilsTest, NormalizePathExpandsUserHomeWhenAvailable) {
+    const auto home = GetTestRoot() / "home";
+    cfgsync::utils::EnsureDirectoryExists(home);
+
+#ifdef _WIN32
+    SetEnvironmentVariable("USERPROFILE", home.string());
+#else
+    SetEnvironmentVariable("HOME", home.string());
+#endif
+
+    const auto normalizedPath = cfgsync::utils::NormalizePath("~/config/file.conf");
+
+    EXPECT_EQ(normalizedPath, (home / "config" / "file.conf").lexically_normal());
+}
+
+TEST_F(PathFileUtilsTest, MakeStorageRelativePathMapsPosixPathUnderFiles) {
+    const auto storagePath = cfgsync::utils::MakeStorageRelativePath("/home/user/.gitconfig");
+
+    EXPECT_EQ(storagePath.generic_string(), "files/home/user/.gitconfig");
+}
+
+TEST_F(PathFileUtilsTest, MakeStorageRelativePathMapsNestedPosixPathUnderFiles) {
+    const auto storagePath = cfgsync::utils::MakeStorageRelativePath("/home/user/.config/nvim/init.lua");
+
+    EXPECT_EQ(storagePath.generic_string(), "files/home/user/.config/nvim/init.lua");
+}
+
+TEST_F(PathFileUtilsTest, MakeStorageRelativePathMapsWindowsDrivePathUnderFiles) {
+    const auto storagePath = cfgsync::utils::MakeStorageRelativePath(R"(C:\Users\Oleksii\.gitconfig)");
+
+    EXPECT_EQ(storagePath.generic_string(), "files/C/Users/Oleksii/.gitconfig");
+}
+
+TEST_F(PathFileUtilsTest, OrdinaryFileValidationAcceptsRegularFile) {
+    const auto filePath = GetTestRoot() / "source.conf";
+    WriteTextFile(filePath, "value=true\n");
+
+    EXPECT_TRUE(cfgsync::utils::IsOrdinaryFile(filePath));
+    EXPECT_NO_THROW(cfgsync::utils::RequireOrdinaryFile(filePath));
+}
+
+TEST_F(PathFileUtilsTest, OrdinaryFileValidationRejectsMissingPath) {
+    const auto filePath = GetTestRoot() / "missing.conf";
+
+    EXPECT_FALSE(cfgsync::utils::IsOrdinaryFile(filePath));
+    EXPECT_THROW(cfgsync::utils::RequireOrdinaryFile(filePath), std::runtime_error);
+}
+
+TEST_F(PathFileUtilsTest, OrdinaryFileValidationRejectsDirectory) {
+    const auto directoryPath = GetTestRoot() / "configs";
+    cfgsync::utils::EnsureDirectoryExists(directoryPath);
+
+    EXPECT_FALSE(cfgsync::utils::IsOrdinaryFile(directoryPath));
+    EXPECT_THROW(cfgsync::utils::RequireOrdinaryFile(directoryPath), std::runtime_error);
+}
+
+TEST_F(PathFileUtilsTest, OrdinaryFileValidationRejectsSymlink) {
+    const auto targetPath = GetTestRoot() / "target.conf";
+    const auto symlinkPath = GetTestRoot() / "linked.conf";
+    WriteTextFile(targetPath, "value=true\n");
+
+    std::error_code errorCode;
+    fs::create_symlink(targetPath, symlinkPath, errorCode);
+    if (errorCode) {
+        GTEST_SKIP() << "Symlink creation is not available in this test environment.";
+    }
+
+    EXPECT_FALSE(cfgsync::utils::IsOrdinaryFile(symlinkPath));
+    EXPECT_THROW(cfgsync::utils::RequireOrdinaryFile(symlinkPath), std::runtime_error);
+}
+
+TEST_F(PathFileUtilsTest, CopyFileCreatesDestinationParentDirectories) {
+    const auto sourcePath = GetTestRoot() / "source.conf";
+    const auto destinationPath = GetTestRoot() / "nested" / "configs" / "source.conf";
+    WriteTextFile(sourcePath, "value=true\n");
+
+    cfgsync::utils::CopyFile(sourcePath, destinationPath);
+
+    EXPECT_TRUE(fs::exists(destinationPath));
+    EXPECT_EQ(ReadTextFile(destinationPath), "value=true\n");
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
