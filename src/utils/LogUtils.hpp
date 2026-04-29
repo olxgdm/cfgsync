2639eb7b4ce7de4ff0b039bb58b3d8a7e7a33df1
Oleksii Gaidadim <agaidadim@gmail.com>
2026-04-29 12:32:19 +0200

    feat: Add basic logging utilities

---
+++ src/utils/LogUtils.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+#include <spdlog/spdlog.h>
+
+namespace cfgsync::utils {
+
+inline void LogDebug(const std::string& message) {
+    spdlog::default_logger_raw()->log(
+        spdlog::level::debug,
+        spdlog::string_view_t{message.data(), message.size()});
+}
+
+inline void LogInfo(const std::string& message) {
+    spdlog::default_logger_raw()->log(
+        spdlog::level::info,
+        spdlog::string_view_t{message.data(), message.size()});
+}
+
+}  // namespace cfgsync::utils
