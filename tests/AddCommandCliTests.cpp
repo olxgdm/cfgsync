f3d6946f760178e1bd4c6d1d5389ddf1765ce372
Oleksii Gaidadim <agaidadim@gmail.com>
2026-05-03 14:59:50 +0200

    feat(add): Add CLI integration tests for 'add' command

---
+++ tests/AddCommandCliTests.cpp
@@ -0,0 +1,102 @@
+#include "common/TestTempDirectory.hpp"
+#include "gtest/gtest.h"
+#include "utils/FileUtils.hpp"
+#include "utils/PathUtils.hpp"
+
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace {
+namespace fs = std::filesystem;
+
+constexpr const char* CfgsyncExecutablePath = CFGSYNC_EXECUTABLE_PATH;
+
+nlohmann::json ReadJsonFile(const fs::path& path) {
+    std::ifstream input{path};
+    nlohmann::json document;
+    input >> document;
+    return document;
+}
+
+void WriteTextFile(const fs::path& path, const std::string& contents) {
+    if (path.has_parent_path()) {
+        cfgsync::utils::EnsureDirectoryExists(path.parent_path());
+    }
+
+    std::ofstream output{path};
+    output << contents;
+}
+
+void SetEnvironmentVariable(const std::string& name, const std::string& value) {
+#ifdef _WIN32
+    _putenv_s(name.c_str(), value.c_str());
+#else
+    setenv(name.c_str(), value.c_str(), 1);
+#endif
+}
+
+std::string QuoteForCommand(const fs::path& path) { return "\"" + path.string() + "\""; }
+
+bool CommandSucceeded(const std::string& command) { return std::system(command.c_str()) == 0; }  // NOSONAR
+
+class AddCommandCliTest : public testing::Test {
+protected:
+    void SetUp() override {
+        TestRoot = cfgsync::tests::MakeTestRoot();
+#ifdef _WIN32
+        SetEnvironmentVariable("APPDATA", (TestRoot / "appdata").string());
+#else
+        SetEnvironmentVariable("HOME", (TestRoot / "home").string());
+#endif
+    }
+
+    void TearDown() override { fs::remove_all(TestRoot); }
+
+    fs::path GetTestRoot() const { return TestRoot; }
+
+private:
+    fs::path TestRoot;
+};
+
+TEST_F(AddCommandCliTest, AddUsesActiveStorageRootPersistedByInit) {
+    const auto storageRoot = GetTestRoot() / "storage";
+    const auto sourcePath = GetTestRoot() / "configs" / ".gitconfig";
+    WriteTextFile(sourcePath, "[user]\n");
+
+    ASSERT_TRUE(CommandSucceeded(QuoteForCommand(CfgsyncExecutablePath) + " init --storage " +
+                                 QuoteForCommand(storageRoot)));
+
+    ASSERT_TRUE(CommandSucceeded(QuoteForCommand(CfgsyncExecutablePath) + " add " + QuoteForCommand(sourcePath)));
+
+    const auto normalizedSourcePath = cfgsync::utils::NormalizePath(sourcePath);
+    const auto document = ReadJsonFile(storageRoot / "registry.json");
+    ASSERT_EQ(document["tracked_files"].size(), 1U);
+    EXPECT_EQ(document["tracked_files"][0]["original_path"], normalizedSourcePath.string());
+    EXPECT_EQ(document["tracked_files"][0]["stored_relative_path"],
+              cfgsync::utils::MakeStorageRelativePath(normalizedSourcePath).generic_string());
+}
+
+TEST_F(AddCommandCliTest, DuplicateAddReturnsSuccessAndLeavesRegistryUnchanged) {
+    const auto storageRoot = GetTestRoot() / "storage";
+    const auto sourcePath = GetTestRoot() / "configs" / ".gitconfig";
+    WriteTextFile(sourcePath, "[user]\n");
+
+    ASSERT_TRUE(CommandSucceeded(QuoteForCommand(CfgsyncExecutablePath) + " init --storage " +
+                                 QuoteForCommand(storageRoot)));
+    ASSERT_TRUE(CommandSucceeded(QuoteForCommand(CfgsyncExecutablePath) + " add " + QuoteForCommand(sourcePath)));
+    const auto documentAfterFirstAdd = ReadJsonFile(storageRoot / "registry.json");
+
+    EXPECT_TRUE(CommandSucceeded(QuoteForCommand(CfgsyncExecutablePath) + " add " + QuoteForCommand(sourcePath)));
+
+    EXPECT_EQ(ReadJsonFile(storageRoot / "registry.json"), documentAfterFirstAdd);
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
