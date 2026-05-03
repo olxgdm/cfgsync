f187237fa401f410ed3e566961700bbcb2a1de63
Oleksii Gaidadim <agaidadim@gmail.com>
2026-05-03 15:37:06 +0200

    test(list): Add unit tests for ListCommand

---
+++ tests/ListCommandTests.cpp
@@ -0,0 +1,110 @@
+#include "commands/ListCommand.hpp"
+#include "common/TestTempDirectory.hpp"
+#include "core/Registry.hpp"
+#include "gtest/gtest.h"
+#include "utils/FileUtils.hpp"
+#include "utils/PathUtils.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <string>
+
+namespace {
+namespace fs = std::filesystem;
+
+nlohmann::json ReadJsonFile(const fs::path& path) {
+    std::ifstream input{path};
+    nlohmann::json document;
+    input >> document;
+    return document;
+}
+
+void WriteTextFile(const fs::path& path, const std::string& contents) {
+    if (path.has_parent_path()) {
+        cfgsync::utils::EnsureDirectoryExists(path.parent_path());
+    }
+
+    std::ofstream output{path};
+    output << contents;
+}
+
+class ListCommandTest : public testing::Test {
+protected:
+    void SetUp() override {
+        TestRoot = cfgsync::tests::MakeTestRoot();
+        Registry_.SetStorageRoot(StorageRoot());
+        Registry_.SetRegistryPath(RegistryPath());
+        Registry_.Save();
+    }
+
+    void TearDown() override { fs::remove_all(TestRoot); }
+
+    fs::path StorageRoot() const { return TestRoot / "storage"; }
+
+    fs::path RegistryPath() const { return StorageRoot() / "registry.json"; }
+
+    fs::path SourcePath(const std::string& filename) const { return TestRoot / "home" / "user" / filename; }
+
+    cfgsync::core::Registry& Registry() { return Registry_; }
+
+private:
+    fs::path TestRoot;
+    cfgsync::core::Registry Registry_;
+};
+
+TEST_F(ListCommandTest, EmptyRegistryPrintsConciseMessage) {
+    const cfgsync::commands::ListCommand command{Registry()};
+    std::ostringstream output;
+
+    command.Execute(output);
+
+    EXPECT_EQ(output.str(), "No files tracked.\n");
+}
+
+TEST_F(ListCommandTest, NonEmptyRegistryPrintsOriginalPathsInRegistryOrder) {
+    const auto firstPath = cfgsync::utils::NormalizePath(SourcePath(".gitconfig"));
+    const auto secondPath = cfgsync::utils::NormalizePath(SourcePath("init.lua"));
+    ASSERT_TRUE(Registry().AddEntry({
+        .OriginalPath = firstPath.string(),
+        .StoredRelativePath = "files/home/user/.gitconfig",
+    }));
+    ASSERT_TRUE(Registry().AddEntry({
+        .OriginalPath = secondPath.string(),
+        .StoredRelativePath = "files/home/user/init.lua",
+    }));
+    const cfgsync::commands::ListCommand command{Registry()};
+    std::ostringstream output;
+
+    command.Execute(output);
+
+    EXPECT_EQ(output.str(), firstPath.string() + "\n" + secondPath.string() + "\n");
+}
+
+TEST_F(ListCommandTest, ExecuteDoesNotMutateOrSaveRegistry) {
+    const auto sourcePath = cfgsync::utils::NormalizePath(SourcePath(".gitconfig"));
+    ASSERT_TRUE(Registry().AddEntry({
+        .OriginalPath = sourcePath.string(),
+        .StoredRelativePath = "files/home/user/.gitconfig",
+    }));
+    Registry().Save();
+    const auto registryBeforeList = ReadJsonFile(RegistryPath());
+
+    WriteTextFile(RegistryPath(), registryBeforeList.dump(4) + "\n");
+    const cfgsync::commands::ListCommand command{Registry()};
+    std::ostringstream output;
+
+    command.Execute(output);
+
+    EXPECT_EQ(ReadJsonFile(RegistryPath()), registryBeforeList);
+    ASSERT_EQ(Registry().GetTrackedEntries().size(), 1U);
+    EXPECT_EQ(Registry().GetTrackedEntries()[0].OriginalPath, sourcePath.string());
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
